f946aca5c81f27d59c2b11b792073a8e9b398acb
ptiwee <reynalb@free.fr>
2016-11-06 10:05:49 +0100

    Add attractmode package

---
+++ package/attractmode/attractmode.mk
@@ -0,0 +1,20 @@
+###############################################################################
+#
+# attractmode
+#
+###############################################################################
+
+ATTRACTMODE_VERSION = v2.1.0
+ATTRACTMODE_SITE = https://github.com/mickelson/attract.git
+ATTRACTMODE_SITE_METHOD = git
+ATTRACTMODE_DEPENDENCIES = sfml-pi ffmpeg
+
+define ATTRACTMODE_BUILD_CMDS
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) PKG_CONFIG="$(PKG_CONFIG_HOST_BINARY)" STRIP="$(TARGET_STRIP)" CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" USE_GLES=1 -C $(@D) prefix=/usr
+endef
+
+define ATTRACTMODE_INSTALL_TARGET_CMDS
+	$(MAKE) PKG_CONFIG="$(PKG_CONFIG_HOST_BINARY)" LD="$(TARGET_LD)" USE_GLES=1 -C $(@D) prefix=$(TARGET_DIR)/usr install
+endef
+	
+$(eval $(generic-package))
