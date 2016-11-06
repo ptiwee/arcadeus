7d5548efe2e79080475337caff5a1d8c0e37c54c
ptiwee <reynalb@free.fr>
2016-11-06 17:36:24 +0100

    Add rpi-userland dependency to sfml-pi

--- package/sfml-pi/sfml-pi.mk
+++ package/sfml-pi/sfml-pi.mk
@@ -9,7 +9,7 @@ SFML_PI_SITE = https://github.com/mickelson/sfml-pi.git
 SFML_PI_SITE_METHOD = git
 
 SFML_PI_INSTALL_STAGING = YES
-SFML_PI_DEPENDENCIES += eudev jpeg openal flac libvorbis freetype
+SFML_PI_DEPENDENCIES += eudev jpeg openal flac libvorbis freetype rpi-userland
 SFML_PI_CONF_OPTS += -DSFML_RPI=1
 
 $(eval $(cmake-package))
