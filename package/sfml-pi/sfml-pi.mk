5716368e71e048a81d2a3a5091a5de2473a42988
ptiwee <reynalb@free.fr>
2016-11-06 09:30:17 +0100

    Add sfml-pi package

---
+++ package/sfml-pi/sfml-pi.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# sfml-pi
+#
+################################################################################
+
+SFML_PI_VERSION = master
+SFML_PI_SITE = https://github.com/mickelson/sfml-pi.git
+SFML_PI_SITE_METHOD = git
+
+SFML_PI_INSTALL_STAGING = YES
+SFML_PI_DEPENDENCIES += eudev jpeg openal flac libvorbis freetype
+SFML_PI_CONF_OPTS += -DSFML_RPI=1
+
+$(eval $(cmake-package))
