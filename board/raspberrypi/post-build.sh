731fe25c782171dc17f7ec05d25d4ef26343710e
ptiwee <reynalb@free.fr>
2016-11-11 13:08:13 +0100

    Mount USB as R/W

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -3,13 +3,13 @@
 set -u
 set -e
 
-# Add a console on tty1
+# Autoload attractmode tty1
 if [ -e ${TARGET_DIR}/etc/inittab ]; then
     grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
 	sed -i '/GENERIC_SERIAL/a\
-tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
+tty1::respawn:/usr/bin/attract -c /mnt/attract # attractmode' ${TARGET_DIR}/etc/inittab
 fi
 
 # Add USB to fstab
 sed -i '/^\/dev\/sda1/d' ${TARGET_DIR}/etc/fstab
-echo '/dev/sda1\t/mnt\tvfat\tdefaults\t0\t0' >> ${TARGET_DIR}/etc/fstab
+echo '/dev/sda1\t/mnt\tvfat\trw\t0\t0' >> ${TARGET_DIR}/etc/fstab
