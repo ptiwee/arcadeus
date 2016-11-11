7e4cfb855e5f06184e15522ae06eec27b8105a4f
ptiwee <reynalb@free.fr>
2016-11-11 12:38:43 +0100

    Add USB to fstab

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -9,3 +9,7 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
+
+# Add USB to fstab
+sed -i '/^\/dev\/sda1/d' ${TARGET_DIR}/etc/fstab
+echo '/dev/sda1\t/mnt\tvfat\tdefaults\t0\t0' >> ${TARGET_DIR}/etc/fstab
