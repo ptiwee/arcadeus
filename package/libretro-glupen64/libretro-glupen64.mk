0860fef213e9eb916a8128a12a4a290a0807fcaf
ptiwee <reynalb@free.fr>
2016-11-29 16:41:52 +0100

    Update libretro-glupen64 version

--- package/libretro-glupen64/libretro-glupen64.mk
+++ package/libretro-glupen64/libretro-glupen64.mk
@@ -4,7 +4,7 @@
 #
 ###############################################################################
 
-LIBRETRO_GLUPEN64_VERSION = 983f2f2
+LIBRETRO_GLUPEN64_VERSION = 2f6e51b
 LIBRETRO_GLUPEN64_SITE = https://github.com/loganmc10/GLupeN64
 LIBRETRO_GLUPEN64_SITE_METHOD = git
 LIBRETRO_GLUPEN64_DEPENDENCIES = retroarch
